88922185e103c7bd8a189d47b5438bd3e0cfd4f4
Pranthor <pranta.nath@trucklagbe.com>
2025-07-23 17:15:14 +0600

    #v0.1 integrated supabase and google login

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,47 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, OnInit, signal} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
+import {supabase} from '../../.vscode/core/services/supabase-auth';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
+  standalone: true,
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+
+  constructor(private router: Router) {
+  }
   protected readonly title = signal('passnplay');
+    async ngOnInit() {
+      const {
+        data: { session },
+      } = await supabase.auth.getSession();
+
+      const user = session?.user;
+
+      if (user) {
+        // Check profile exists
+        const { data: profile, error } = await supabase
+          .from('profiles')
+          .select('*')
+          .eq('id', user.id)
+          .single();
+
+        if (error && error.code === 'PGRST116') {
+          // No profile, insert
+          await supabase.from('profiles').insert([
+            {
+              id: user.id,
+              full_name: user.email,
+              role: 'viewer',
+            },
+          ]);
+        }
+
+        this.router.navigate(['/dashboard']);
+      }
+    }
+
 }
---
+++ src/app/auth/login/login.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {SupabaseAuth,supabase} from '../../../../.vscode/core/services/supabase-auth';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './login.html',
+  standalone: true,
+  styleUrl: './login.css'
+})
+export class Login {
+  loginForm: FormGroup;
+  loading = false;
+  errorMsg!: string;
+  private router! : Router;
+
+  constructor(private fb: FormBuilder,
+                private supabaseAuth: SupabaseAuth) {
+      this.loginForm = this.fb.group({
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+      });
+    }
+
+  async loginWithGoogle() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+    });
+
+    if (error) console.error('Google login error:', error.message);
+  }
+
+
+}
