3c85bd3f7463db6321b8e030fe02801853d3a5df
Pranthor <pranta.nath@trucklagbe.com>
2025-07-24 17:22:45 +0600

    #v0.1 added profile info into table

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {supabase} from '../../../.vscode/core/services/supabase-auth';
 import {NgClass, NgIf} from '@angular/common';
@@ -15,10 +15,45 @@ import {NgClass, NgIf} from '@angular/common';
   standalone: true,
   styleUrl: './dashboard.css'
 })
-export class Dashboard {
+export class Dashboard implements OnInit {
   sidebarOpen : boolean = false;
   constructor(private router: Router) {
   }
+
+  async ngOnInit() {
+    await this.fetchUserProfile();
+  }
+
+  async fetchUserProfile() {
+    const {
+      data: { user },
+      error,
+    } = await supabase.auth.getUser();
+
+    if (error) {
+      console.error('Error fetching user:', error.message);
+      return;
+    }
+
+    if (user) {
+      const fullName = user.user_metadata?.['full_name'];
+      console.log(user);
+        const { error: insertError } = await supabase.from('profiles').insert([
+          {
+            user_id: user.id,
+            fullName: fullName || '', // fallback to empty string if not found
+          },
+        ]);
+
+        if (insertError) {
+          console.error('Error inserting profile:', insertError.message);
+        } else {
+          console.log('✅ Profile created for first-time login');
+        }
+      }
+  }
+
+
   async logout() {
     const { error } = await supabase.auth.signOut();
     if (error) {
@@ -27,4 +62,6 @@ export class Dashboard {
     }
     this.router.navigate(['/login']);
   }
+
+
 }
